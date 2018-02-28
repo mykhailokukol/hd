4e3457bebfa43296b1a62addcb89e654dbb3f3d8
Mykhailo <mykhailokukol@gmail.com>
2018-02-28 18:39:53 +0200

    elemtary server

--- server/main.js
+++ server/main.js
@@ -1,1 +1,8 @@
-//example
+const express = require('express');
+let server = express();
+
+server.get('/', (req, res) => res.send('hello world'));
+
+server.listen(3000, function () {
+  console.log('Сервер запущен');
+})
